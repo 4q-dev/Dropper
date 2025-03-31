ed45f846ff9f45ae81bc64a05c62c8fe8ffbdbaf
Alexandr <bagnencko.sasha1@gmail.com>
2025-03-31 20:49:59 +0300

    +w za architecture

---
+++ Backend/src/Presentation/Dropper.Presentation/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿namespace Dropper.Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+
+    public static void ConfigureWebApplication(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
